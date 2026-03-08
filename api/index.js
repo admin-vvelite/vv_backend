08775c9851a6cf28f9ba8864205f0701799fd5e5
KamelMohamed <kamel.mohamed.zidan.22@gmail.com>
2026-03-08 03:11:38 +0200

    MyFatoorah Update

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const home_routes = require("../routes/home_routes");
 const odoo_routes = require("../routes/odoo_routes");
-
+const myFatoorah_routes = require("../routes/myFatoorah_routes");
 
 // Use CORS once with options
 app.use(
@@ -14,12 +14,20 @@ app.use(
     origin: "*", // Allow all origins (adjust for production)
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-  })
+  }),
 );
 
-app.use(express.json());
+// Replace app.use(express.json()); with this:
+app.use(
+  express.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf; // This captures the exact string sent by myFatoorah
+    },
+  }),
+);
 app.use("/api/v1/home", home_routes);
 app.use("/api/v1/odoo", odoo_routes);
+app.use("/api/v1/myfatoorah", myFatoorah_routes);
 
 app.get("/", (req, res) => {
   res.send("Server is running. Go to /data to see the API.");
--- routes/home_routes.js
+++ routes/home_routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { data_controller } = require('../controllers/home_controller');
 
 // Changed to GET to match the standard behavior for fetching data
-router.get("/data", data_controller);
+router.get("/home", data_controller);
 
 module.exports = router;
